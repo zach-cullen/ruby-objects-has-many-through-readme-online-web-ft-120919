c578876d2b4a40857d84056d028ef5f5e1929f83
Zach Cullen <zacullen@gmail.com>
2020-01-07 19:35:28 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -8,6 +8,18 @@ class Waiter
     @experience = experience
     @@all << self
   end 
+  
+  def new_meal(customer, total, tip)
+    order = Meal.new(self, customer, total, tip)
+  end
+  
+  def meals
+    Meals.all.select {|meal| meal.waiter == self}
+  end
+  
+  def best_tipper
+    self.meals.
+  end
     
   def self.all
     @@all
